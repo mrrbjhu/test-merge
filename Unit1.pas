f2cac6ad3ad01cb74e1b481e29fc9ea868812fe2
krka <krka92@bk.ru>
2018-12-25 12:48:45 +0700

    * testunit1

--- Unit1.pas
+++ Unit1.pas
@@ -4,10 +4,13 @@ interface
 
 uses
   Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
-  Vcl.Controls, Vcl.Forms, Vcl.Dialogs;
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.StdCtrls, TestUnit1;
 
 type
   TForm1 = class(TForm)
+    Button1: TButton;
+    procedure Button1Click(Sender: TObject);
+    procedure FormCreate(Sender: TObject);
   private
     { Private declarations }
   public
@@ -16,9 +19,20 @@ type
 
 var
   Form1: TForm1;
+  u1: TTestClass1;
 
 implementation
 
 {$R *.dfm}
+
+procedure TForm1.Button1Click(Sender: TObject);
+begin
+  MessageDlg(u1.GetFieldOfString, mtInformation, [mbOk], 0);
+end;
+
+procedure TForm1.FormCreate(Sender: TObject);
+begin
+  u1 := TTestClass1.Create(100);
+end;
 
 end.
